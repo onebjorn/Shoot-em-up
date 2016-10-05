262a365e16bdf569e30d8bce0299d73deb361075
thecaptainuniverse <stasromanikhin@gmail.com>
2016-10-06 01:45:17 +0300

    up

--- src/ray2d_maybe.h
+++ src/ray2d_maybe.h
@@ -10,23 +10,6 @@ using namespace std;
 
 float const pi = 3.14159265;
 float const kEps=1e-5f;
-=======
-class Point
-
-{
-public:
-        Point()=default;
-
-        Point(Point const & obj)
-                : m_x(obj.m_x), m_y(obj.m_y)
-        {}
-
-        Point(float x, float y)
-                : m_x(x), m_y(y)
-        {}
-
-        float & x() {return m_x;}
-        float & y() {return m_y;}
 
 class Ray
 {
