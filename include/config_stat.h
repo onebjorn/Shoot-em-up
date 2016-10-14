a870a744088644864f8bc46cbc25d714c324ddf6
thecaptainuniverse <stasromanikhin@gmail.com>
2016-10-14 19:56:32 +0300

    add config

---
+++ include/config_stat.h
@@ -0,0 +1,9 @@
+/* Различные константы игры: здоровье, скорость, начальные координаты, всякая всячина типа урона*/
+#define AlienSizeX  (1.0f)
+#define AlienSizeY  (1.0f)
+#define AlienHealth (100.0f)
+#define AlienSpeed (10.0f)
+
+#define ObstacleSizeX  (4.0f)
+#define ObstacleSizeY  (0.5f)
+#define ObstacleHealth (10.0f)
