138ccbc267d0cdd0e7008e0ed4381853a08b93a5
randomNaN <de.kobelev.alex@gmail.com>
2016-10-15 02:11:59 +0300

    Added parameters of bullet and gun

--- include/config_stat.h
+++ include/config_stat.h
@@ -7,3 +7,11 @@
 #define ObstacleSizeX  (4.0f)
 #define ObstacleSizeY  (0.5f)
 #define ObstacleHealth (10.0f)
+
+#define GunHealth (100.0f)
+#define GunSpeed (8.0f)
+#define GunPower (3.0f)
+#define GunAmmo (10)
+
+#define BulletSpeed (200.0f)
+#define BulletDamage (100.0f)
