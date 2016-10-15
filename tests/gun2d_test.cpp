a85f2f9752e87789918bc957db7d7a08eefd32f6
randomNaN <de.kobelev.alex@gmail.com>
2016-10-15 21:04:59 +0300

    Add test 4 gun

--- tests/gun2d_test.cpp
+++ tests/gun2d_test.cpp
@@ -1,1 +1,44 @@
+#include "gtest/gtest.h"
+#include "../include/config_stat.h"
+#include "gun.h"
+#include <sstream>
+#include <iostream>
+#include <unordered_set>
 
+TEST(Gun2D_test, test_construction)
+{
+  Point2D p1 = { 5.0f, 5.0f };
+  Point2D p2 = { 2.0f, 3.0f };
+  Box2D b1 = { 1.5f, 3.0f, 5.5f, 12.5f };
+
+  Gun2D a1(p1, p2);
+  EXPECT_EQ(a1.x2(), 2.0f);
+  EXPECT_EQ(a1.y2(), 3.0f);
+  EXPECT_EQ(a1.x1(), 5.0f);
+  EXPECT_EQ(a1.y1(), 5.0f);
+  EXPECT_EQ(a1.GimmeSpeed(), GunSpeed);
+  EXPECT_EQ(a1.GimmeAmmo(), GunAmmo);
+  EXPECT_EQ(a1.GimmeHealth(), GunHealth);
+  EXPECT_EQ(a1.GimmeRate(), GunPower);
+
+  Gun2D a2(1.5f, 1.0f, 2.5f, 5.0f);
+  EXPECT_EQ(a2.x2(), 2.5f);
+  EXPECT_EQ(a2.x1(), 1.5f);
+  EXPECT_EQ(a2.y1(), 1.0f);
+  EXPECT_EQ(a2.y2(), 5.0f);
+  EXPECT_EQ(a2.GimmeSpeed(), GunSpeed);
+  EXPECT_EQ(a2.GimmeAmmo(), GunAmmo);
+  EXPECT_EQ(a2.GimmeHealth(), GunHealth);
+  EXPECT_EQ(a2.GimmeRate(), GunPower);
+
+  Gun2D a3(b1);
+  EXPECT_EQ(a3.x1(), 1.5f);
+  EXPECT_EQ(a3.y1(), 3.0f);
+  EXPECT_EQ(a3.x1(), 5.5f);
+  EXPECT_EQ(a3.x1(), 12.5f);
+  EXPECT_EQ(a3.GimmeSpeed(), GunSpeed);
+  EXPECT_EQ(a3.GimmeAmmo(), GunAmmo);
+  EXPECT_EQ(a3.GimmeHealth(), GunHealth);
+  EXPECT_EQ(a3.GimmeRate(), GunPower);
+
+}
