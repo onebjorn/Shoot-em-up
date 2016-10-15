ca56e5fdda432335d376ccf37348041b6bf575b6
randomNaN <de.kobelev.alex@gmail.com>
2016-10-16 00:43:31 +0300

    rewrite

--- src/bullet.h
+++ src/bullet.h
@@ -2,33 +2,36 @@
 #include "../include/config_stat.h"
 #include "ray2d.h"
 
-class Bullet2D
+class Bullet2D : public Ray2D
 {
 public:
   Bullet2D() = default;
 
   Bullet2D(float const x0, float const y0, float const x1, float const y1)
-    : Ray2D(x0,y0,x1,y1)
-    , m_bulletspeed(BulletSpeed)
-    , m_bulletdam(BulletDamage)
+    : Ray2D( x0 ,y0 ,x1 ,y1 )
+    , m_bulletSpeed(BulletSpeed)
+    , m_bulletDam(BulletDamage)
   {}
 
   Bullet2D(Ray2D const & bullobj)
-    : Ray2D(bullobj)
-    , m_bulletspeed(BulletSpeed)
-    , m_bulletdam(BulletDamage)
+    : Ray2D( bullobj )
+    , m_bulletSpeed(BulletSpeed)
+    , m_bulletDam(BulletDamage)
   {}
 
   Bullet2D(initializer_list<float> const & lst)
-    : Ray2D(lst)
-    , m_bulletspeed(BulletSpeed)
-    , m_bulletdam(BulletDamage)
+    : Ray2D( lst )
+    , m_bulletSpeed(BulletSpeed)
+    , m_bulletDam(BulletDamage)
   {}
+
+  float const & GimmeDamage() const { return m_bulletDam; }
+  float const & GimmeSpeed() const { return m_bulletSpeed; }
 
   //TODO:Hit the object
 
 private:
   //TODO: Size of bullet
-  float m_bulletspeed = 0.0f;
-  float m_bulletdam = 0.0f;
+  float m_bulletSpeed = 0.0f;
+  float m_bulletDam = 0.0f;
 };
