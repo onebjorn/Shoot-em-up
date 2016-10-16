12f49855fa39564ad43d073558e58cfbbcf0f849
randomNaN <de.kobelev.alex@gmail.com>
2016-10-16 17:23:03 +0300

    Update tests

--- tests/bullet2d_test.cpp
+++ tests/bullet2d_test.cpp
@@ -5,32 +5,39 @@
 #include <iostream>
 #include <unordered_set>
 
-TEST(Bullet2D_test, test_construction)
+TEST(bullet2d_test, test_construction)
 {
-  Ray2D p1 = { 5.0f, 5.0f, pi()/3.0f };
+  Box2D p1 = { 5.0f, 5.0f, 10.0f, 10.0f};
 
   Bullet2D a1(p1);
-  EXPECT_EQ(a1.x0(), 5.0f);
-  EXPECT_EQ(a1.y0(), 5.0f);
-  EXPECT_EQ(a1.GetDirect(0), 0.5f);
-  EXPECT_EQ(a1.GetDirect(1), 0.866f);
+  EXPECT_EQ(a1.x1(), 5.0f);
+  EXPECT_EQ(a1.y1(), 5.0f);
+  EXPECT_EQ(a1.x2(), 10.0f);
+  EXPECT_EQ(a1.y2(), 10.0f);
   EXPECT_EQ(a1.GimmeSpeed(), BulletSpeed);
   EXPECT_EQ(a1.GimmeDamage(), BulletDamage);
 
   Bullet2D a2(1.0f, 1.0f, 4.0f, 5.0f);
-  EXPECT_EQ(a2.x0(), 1.0f);
-  EXPECT_EQ(a2.y0(), 1.0f);
-  EXPECT_EQ(a2.GetDirect(0), 0.6f);
-  EXPECT_EQ(a2.GetDirect(1), 0.8f);
+  EXPECT_EQ(a2.x1(), 1.0f);
+  EXPECT_EQ(a2.y1(), 1.0f);
+  EXPECT_EQ(a2.x2(), 4.0f);
+  EXPECT_EQ(a2.y2(), 5.0f);
   EXPECT_EQ(a1.GimmeSpeed(), BulletSpeed);
   EXPECT_EQ(a1.GimmeDamage(), BulletDamage);
 
   Bullet2D a3 = { 1.0f, 1.0f, 4.0f, 5.0f };
   EXPECT_EQ(a3.x1(), 1.0f);
   EXPECT_EQ(a3.y1(), 1.0f);
-  EXPECT_EQ(a3.x1(), 0.6f);
-  EXPECT_EQ(a3.x1(), 0.8f);
+  EXPECT_EQ(a3.x2(), 4.0f);
+  EXPECT_EQ(a3.y2(), 5.0f);
   EXPECT_EQ(a1.GimmeSpeed(), BulletSpeed);
   EXPECT_EQ(a1.GimmeDamage(), BulletDamage);
+}
 
+TEST(bullet2d_test, test_move)
+{
+  Bullet2D bul1(1.0f, 2.0f, 3.0f, 4.0f);
+  Bullet2D bul2 = move(bul1);
+  EXPECT_EQ(bul1.x1(), 1.0f);
+  EXPECT_EQ(bul2.x1(), 0.0f);
 }
--- tests/gun2d_test.cpp
+++ tests/gun2d_test.cpp
@@ -5,25 +5,25 @@
 #include <iostream>
 #include <unordered_set>
 
-TEST(Gun2D_test, test_construction)
+TEST(gun2d_test, test_construction)
 {
   Point2D p1 = { 5.0f, 5.0f };
   Point2D p2 = { 2.0f, 3.0f };
   Box2D b1 = { 1.5f, 3.0f, 5.5f, 12.5f };
 
   Gun2D a1(p1, p2);
-  EXPECT_EQ(a1.x2(), 2.0f);
-  EXPECT_EQ(a1.y2(), 3.0f);
-  EXPECT_EQ(a1.x1(), 5.0f);
-  EXPECT_EQ(a1.y1(), 5.0f);
+  EXPECT_EQ(a1.x2(), 5.0f);
+  EXPECT_EQ(a1.y2(), 5.0f);
+  EXPECT_EQ(a1.x1(), 2.0f);
+  EXPECT_EQ(a1.y1(), 3.0f);
   EXPECT_EQ(a1.GimmeSpeed(), GunSpeed);
   EXPECT_EQ(a1.GimmeAmmo(), GunAmmo);
   EXPECT_EQ(a1.GimmeHealth(), GunHealth);
   EXPECT_EQ(a1.GimmeRate(), GunPower);
 
   Gun2D a2(1.5f, 1.0f, 2.5f, 5.0f);
-  EXPECT_EQ(a2.x2(), 2.5f);
   EXPECT_EQ(a2.x1(), 1.5f);
+  EXPECT_EQ(a2.x2(), 2.5f);
   EXPECT_EQ(a2.y1(), 1.0f);
   EXPECT_EQ(a2.y2(), 5.0f);
   EXPECT_EQ(a2.GimmeSpeed(), GunSpeed);
@@ -34,11 +34,10 @@ TEST(Gun2D_test, test_construction)
   Gun2D a3(b1);
   EXPECT_EQ(a3.x1(), 1.5f);
   EXPECT_EQ(a3.y1(), 3.0f);
-  EXPECT_EQ(a3.x1(), 5.5f);
-  EXPECT_EQ(a3.x1(), 12.5f);
+  EXPECT_EQ(a3.x2(), 5.5f);
+  EXPECT_EQ(a3.y2(), 12.5f);
   EXPECT_EQ(a3.GimmeSpeed(), GunSpeed);
   EXPECT_EQ(a3.GimmeAmmo(), GunAmmo);
   EXPECT_EQ(a3.GimmeHealth(), GunHealth);
   EXPECT_EQ(a3.GimmeRate(), GunPower);
-
 }
