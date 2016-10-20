2355d3e46fff574dc78e5bc12e3d3232d907fee5
thecaptainuniverse <stasromanikhin@gmail.com>
2016-10-20 22:48:11 +0300

    add Bullets

---
+++ src/bulletsManager.h
@@ -0,0 +1,25 @@
+#include "alien.h"
+#include "gun.h"
+#include "bullet.h"
+#include <vector>
+#include "../include/config_stat.h"
+
+using Bullets = std::vector<Bullet>;
+
+class BulletsManager
+{
+public:
+
+  BulletsManager() = default;
+
+  Bullets const & getBullets() { return m_bullets; }
+
+  void AddBullet(Bullet const & obj) { m_bullets.push_back(obj); }
+
+  ~BulletsManager() = default;
+
+private:
+
+  std::vector<Bullet> m_bullets;
+
+};
