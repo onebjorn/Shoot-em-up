87d257efa72ef54061629d22b93a17681f96d45b
Stas Romanikhin <thecaptainuniverse@users.noreply.github.com>
2016-11-15 00:55:20 +0400

    Update gameEntity.h

--- src/gameEntity.h
+++ src/gameEntity.h
@@ -3,6 +3,17 @@
 #include "box2d.h"
 #include "ray2d.h"
 #include "../include/config_stat.h"
+#include <memory>
+
+
+class Factory
+{
+public:
+  template<typename T, typename... Args> std::unique_ptr<T> Create(Args && ... args)
+  {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+  }
+};
 
 class GameEntity
 {
