bf89d79c27f00e78ea3a384a2eb727ce9d690bbf
thecaptainuniverse <stasromanikhin@gmail.com>
2016-11-16 23:34:50 +0300

    clean

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,4 @@
-#include <QApplication>
-#include <QMainWindow>
-#include <QSurfaceFormat>
-#include "main_window.hpp"
-
-int main(int argc, char ** argv)
+int main()
 {
-  QApplication a(argc, argv);
-
-  QSurfaceFormat format;
-  format.setDepthBufferSize(24);
-  format.setStencilBufferSize(8);
-  QSurfaceFormat::setDefaultFormat(format);
-
-  MainWindow mw;
-  mw.show();
-  return a.exec();
+  return 0;
 }
