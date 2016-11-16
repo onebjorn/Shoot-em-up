67fafb7a07af3da1afc8d33c8d4cb7cad1abe934
thecaptainuniverse <stasromanikhin@gmail.com>
2016-11-16 21:23:29 +0300

    add Qt and MainWindow

--- src/logger.h
+++ src/logger.h
@@ -2,26 +2,9 @@
 #include <iostream>
 #include <ostream>
 #include <fstream>
+#include "singleton.h"
 
 using namespace std;
-
-template<typename T> class Singleton
-{
-public:
-  static T & Instance()
-  {
-    static T inst;
-    return inst;
-  }
-
-protected:
-  Singleton() = default;
-  virtual ~Singleton() = default;
-  Singleton(Singleton const &) = delete;
-  Singleton & operator = (Singleton const &) = delete;
-  Singleton(Singleton &&) = delete;
-  Singleton & operator = (Singleton &&) = delete;
-};
 
 class Logger : public Singleton <Logger>
 {
