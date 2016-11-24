12e9a6acd08ceb36d25041f92b52a1830a481aa8
randomNaN <de.kobelev.alex@gmail.com>
2016-11-24 18:15:57 +0300

    Added Json and log file writing

--- src/game/logger.h
+++ src/game/logger.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <ostream>
 #include <fstream>
+#include <time.h>
 #include "singleton.h"
 
 using namespace std;
@@ -9,12 +10,24 @@ using namespace std;
 class Logger : public Singleton <Logger>
 {
 public:
-
   template <typename T>
   ostream & Log(T obj, ostream & os)
   {
     os << obj;
     return os;
+  }
+
+  template <typename T>
+  fstream & Log(T obj)
+  {
+    setTime();
+    file.open("Logfile.txt", ios_base::out | ios_base::app);
+    if (file.is_open())
+      {
+        file << asctime(m_timeinfo) << "\t" << obj << endl;
+      }
+    else cout << "Unable to open" << endl;
+    file.close();
   }
 
   template<typename T, template<typename, typename...> class C, typename... Args>
@@ -25,18 +38,41 @@ public:
     return os;
   }
 
+  template<typename T, template<typename, typename...> class C, typename... Args>
+  fstream & Log(C<T, Args...> const & objs)
+  {
+    setTime();
+    file.open("Logfile.txt", ios_base::out | ios_base::app);
+    if (file.is_open())
+      {
+        for (auto const & obj: objs)
+          file << asctime(m_timeinfo) << "\t" << obj << endl;
+      }
+    else cout << "Unable to open" << endl;
+    file.close();
+  }
+
   /*TODO добавить переключатель*/
   void LoggerSwitchOn() { m_logSwitch = true; }
   void LoggerSwitchOff() { m_logSwitch = false; }
 
   void LoggerSwitchFileOn() { m_logSwitchFile = true; }
   void LoggerSwitchFileOff() { m_logSwitchFile = false; }
+  void setTime()
+  {
+    time(& m_rawtime);
+    m_timeinfo = localtime( & m_rawtime);
+  }
 
 private:
   bool m_logSwitch = true; /* Переключатель логгера(1 запись, 0 - не запись)*/
   bool m_logSwitchFile = true; /* Перключатель запись в файл */
+  Logger() = default;
 
   friend class Singleton<Logger>;
-  Logger() = default;
+
+  fstream file;
+  time_t m_rawtime;
+  struct tm * m_timeinfo;
 
 };
